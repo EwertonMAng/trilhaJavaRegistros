ddc54be59bee556fd149ed3ae43ad4127dffe81e
Ewerton <ewertonangelodev@gmail.com>
2023-01-29 03:53:38 -0300

    Finalizando Exceções

--- bytebank-herdado-conta/src/ContaCorrente.java
+++ bytebank-herdado-conta/src/ContaCorrente.java
@@ -9,9 +9,9 @@ public class ContaCorrente extends Conta implements Tributavel {
 	}
 	
 	@Override
-	public boolean saca(double valor) {
+	public void saca(double valor) throws SaldoInsuficienteException {
 		double saquecomtaxa = valor + 0.2;
-		return super.saca(saquecomtaxa);
+		super.saca(saquecomtaxa);
 	}
 
 	@Override
--- bytebank-herdado-conta/src/TesteContas.java
+++ bytebank-herdado-conta/src/TesteContas.java
@@ -1,7 +1,12 @@
 
 public class TesteContas {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SaldoInsuficienteException {
+		
+		
+		
+		ContaCorrente outraa = null;
+		outraa.deposita(200.0);
 		
 		ContaCorrente cc = new ContaCorrente(111, 111);
 		cc.deposita(100.0);
